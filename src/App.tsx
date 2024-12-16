fb233a78ebaabcc1eb8647a5509a3ad73638120e
Eduardo VR <evenegas06@hotmail.com>
2024-12-16 00:22:53 -0600

    header.tsx... #108

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
   return (
     <>
-      Hola mundo
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">
+          Calculadora de Propinas y Consumo
+        </h1>
+      </header>
     </>
   )
 }
