e0e71906a301831034f7362d9cb6a807a593bcee
Eduardo VR <evenegas06@hotmail.com>
2024-12-24 20:38:50 -0600

    add elements at order state

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -4,14 +4,20 @@ import type { MenuItem, OrderItem } from '../types';
 export const useOrder = () => {
 	const [order, setOrder] = useState<OrderItem[]>([]);
 
-    /**
-     *  Add an item to the order.
-     */
+	/**
+	 *  Add an item to the order.
+	 */
 	const addItem = (item: MenuItem) => {
-		console.log(item);
+		// const new_item = structuredClone(item);
+		// new_item.quantity = 1;
+
+		const new_item = { ...item, quantity: 1 };
+		setOrder([...order, new_item]);
 	};
 
+    console.log(order);
+
 	return {
-        addItem,
-    };
+		addItem,
+	};
 };
